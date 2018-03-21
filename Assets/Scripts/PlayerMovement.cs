3183a6732238e50ca364254fb86e1c67c6e2917d
C.Rondeau <rondeau.clement@gmail.com>
2018-03-22 00:19:44 +0100

    Add wall jump behavior

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,15 +6,28 @@ public class PlayerMovement : MonoBehaviour {
 
     public float horizontalSpeed = 10f;
     public float jumpHeight = 2f;
+    public float wallJumpHeight = 2f;
     public float jumpTimeToApex = 0.25f;
+    public float wallJumpHorizontalForce = 0.25f;
 
     private float playerWidth = 0f;
     private float playerHeight = 0f;
     private Rigidbody2D rb2d;
     private PlayerBehaviour _player;
+    private bool _grounded = true;
     private bool _canJump = true;
-
+    private bool _wallDetected = false;
+    private Side _touchingSide = Side.none;
+    
     private float _lastJumpTime = 0f;
+    private float _lastWallJumpTime = 0f;
+
+    private enum Side
+    {
+        right,
+        left,
+        none
+    }
 
     private void Awake()
     {
@@ -39,6 +52,14 @@ public class PlayerMovement : MonoBehaviour {
                 {
                     Jump();
                 }
+                else
+                {
+                    // If the player is in air, touching a wall, they can jump
+                    if (_wallDetected && !_grounded)
+                    {
+                        WallJump(_touchingSide);
+                    }
+                }
             }
         }
     }
@@ -48,21 +69,32 @@ public class PlayerMovement : MonoBehaviour {
         if (!GameManager.Instance.gamePaused)
         {
             // Reset speed and apply new speed
-            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
-
             float xAxis = Input.GetAxisRaw("L_XAxis_" + _player.id);
 
-            if (xAxis != 0f)
+            if (Time.time >= _lastWallJumpTime + 0.05f)
             {
-                rb2d.velocity = new Vector2(horizontalSpeed * xAxis, rb2d.velocity.y);
+                rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+                if (xAxis != 0f)
+                {
+                    rb2d.velocity = new Vector2(horizontalSpeed * xAxis, rb2d.velocity.y);
+                }
+            }
+            else
+            {
+                rb2d.velocity = new Vector2(rb2d.velocity.x, rb2d.velocity.y);
             }
 
             // Check if the player collide with a wall on one side
+            _wallDetected = false;
+            _touchingSide = Side.none;
+
             RaycastHit2D[] rightSideHits = Physics2D.RaycastAll(new Vector2(transform.position.x, transform.position.y + ((2f / 3f) * playerHeight / 2f)), new Vector2(1, 0), (playerWidth / 2f) + 0.2f);
             foreach (var hit in rightSideHits)
             {
                 if (hit.collider != null && !hit.collider.gameObject.CompareTag(GameManager.Instance.playerTag))
                 {
+                    _touchingSide = Side.right;
+                    _wallDetected = true;
                     // Debug contact point
                     if (GameManager.Instance.debugMode)
                     {
@@ -83,6 +115,8 @@ public class PlayerMovement : MonoBehaviour {
             {
                 if (hit.collider != null && !hit.collider.gameObject.CompareTag(GameManager.Instance.playerTag))
                 {
+                    _touchingSide = Side.left;
+                    _wallDetected = true;
                     // Debug contact point
                     if (GameManager.Instance.debugMode)
                     {
@@ -98,6 +132,7 @@ public class PlayerMovement : MonoBehaviour {
                                Color.red);
             }
 
+            _grounded = false;
             // Check if the player is in air
             if (!_canJump && Time.time >= _lastJumpTime + 0.05f)
             {
@@ -115,6 +150,7 @@ public class PlayerMovement : MonoBehaviour {
                     {
                         Debug.Log(hit.collider.name);
                         _canJump = true;
+                        _grounded = true;
 
                         // Debug contact point
                         if (GameManager.Instance.debugMode)
@@ -134,5 +170,25 @@ public class PlayerMovement : MonoBehaviour {
         rb2d.AddForce(new Vector2(0, (jumpHeight - (0.5f * Physics2D.gravity.y * rb2d.gravityScale * jumpTimeToApex * jumpTimeToApex) / jumpTimeToApex)), ForceMode2D.Impulse);
 
         _lastJumpTime = Time.time;
+    }
+
+    private void WallJump(Side side)
+    {
+        print("Wall jump");
+        switch (side)
+        {
+            case Side.right:
+                rb2d.AddForce(new Vector2(-wallJumpHorizontalForce, (wallJumpHeight - (0.5f * Physics2D.gravity.y * rb2d.gravityScale * jumpTimeToApex * jumpTimeToApex) / jumpTimeToApex)), ForceMode2D.Impulse);
+                _lastWallJumpTime = Time.time;
+                break;
+
+            case Side.left:
+                rb2d.AddForce(new Vector2(wallJumpHorizontalForce, (wallJumpHeight - (0.5f * Physics2D.gravity.y * rb2d.gravityScale * jumpTimeToApex * jumpTimeToApex) / jumpTimeToApex)), ForceMode2D.Impulse);
+                _lastWallJumpTime = Time.time;
+                break;
+
+            case Side.none:
+                break;
+        }
     }
 }
